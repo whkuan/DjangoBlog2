50b2950c1d0d6f7777984ccdd8b1ea79659bbefb
Whkuan <kkuanww@gmail.com>
2025-06-12 13:23:43 +0800

    A | add view function.

--- DjangoBlog/urls.py
+++ DjangoBlog/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-
+from posts.views import index
+from posts.views import about
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", index),
+    path("about", about),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from posts.models import Post
+
 
 # Create your views here.
+
+
+
+
+def index(request):
+    article_records = Post.objects.all()
+    article_list = list()
+   
+   
+    for count, article in enumerate(article_records):
+        article_list.append("#{}: {}<br>".format(str(count), str(article)))
+        article_list.append("<small>{}</small><br><hr>".format(article.content))
+   
+   
+    return HttpResponse(article_list)
+
+
+def about(request):
+    return HttpResponse("hello world")
